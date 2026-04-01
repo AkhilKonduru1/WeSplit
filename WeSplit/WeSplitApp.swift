9b4bbf2af53d03d1bd1eb909f4d6b2e2637f25c5
Akhil Konduru <akhilkonduru23@gmail.com>
2026-04-01 18:44:19 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Akhil Konduru on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
